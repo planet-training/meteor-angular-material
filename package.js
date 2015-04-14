606bd2489740c04a87fde4840861ee53d85ac2a9
Dominic Garms <djgarms@gmail.com>
2015-04-15 07:37:52 +0800

    switch to bower and new 0.9.0-rc1 release

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: "Angular-Material components",
   name: "planettraining:angular-material",
-  version: "0.0.1",
+  version: "0.0.2",
   git: "https://github.com/planet-training/meteor-angular-material.git"
 });
 
@@ -11,9 +11,9 @@ Package.onUse(function (api) {
 
   api.addFiles([
     /* material-angular base packages */
-    'node_modules/angular-animate/angular-animate.min.js',
-    'node_modules/angular-aria/angular-aria.min.js',
-    'node_modules/angular-material/angular-material.js',
-    'node_modules/angular-material/angular-material.min.css'
+    'bower_components/angular-animate/angular-animate.min.js',
+    'bower_components/angular-aria/angular-aria.min.js',
+    'bower_components/angular-material/angular-material.min.js',
+    'bower_components/angular-material/angular-material.min.css'
   ], "client");
 });
