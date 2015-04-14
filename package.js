2c3f9451dda80a97494bd053d7d9f3158788b379
Dominic Garms <djgarms@gmail.com>
2015-04-14 11:37:29 +0800

    switch to npm module

--- package.js
+++ package.js
@@ -1,26 +1,20 @@
-Npm.depends({
-  "angular-animate": "1.3.15",
-  "angular-material": "0.8.3"
-});
-
 Package.describe({
   // Short two-sentence summary.
   summary: "Angular Material components",
+  name: "planettraining:meteor-angular-material",
   // Version number.
-  version: "0.0.1"
+  version: "0.0.2"
 });
 
 Package.onUse(function (api) {
 
-  api.use("urigo:angular", ["client", "server"]);
+  api.use("urigo:angular@0.8.4", ["client", "server"]);
 
   api.addFiles([
     /* material-angular base packages */
-    '.npm/package/node_modules/angular-animate/angular-animate.min.js',
-    '.npm/package/node_modules/angular-aria/angular-aria.min.js',
-    '.npm/package/node_modules/angular-material/angular-material.js',
-    '.npm/package/node_modules/angular-material/angular-material.min.css'
+    'node_modules/angular-animate/angular-animate.min.js',
+    'node_modules/angular-aria/angular-aria.min.js',
+    'node_modules/angular-material/angular-material.js',
+    'node_modules/angular-material/angular-material.min.css'
   ], "client");
-
-
 });
