bb8665e8ad32bba3497244eeecd94009175e0be1
Dominic Garms <djgarms@gmail.com>
2015-04-20 10:42:10 +0800

    make us of angularjs packages

--- package.js
+++ package.js
@@ -1,19 +1,18 @@
 Package.describe({
   summary: "Angular-Material components",
   name: "planettraining:angular-material",
-  version: "0.0.2",
+  version: "0.0.3",
   git: "https://github.com/planet-training/meteor-angular-material.git"
 });
 
 Package.onUse(function (api) {
 
-  api.use("urigo:angular@0.8.4", ["client", "server"]);
+  api.use(["angularjs:angular@1.3.15", "angularjs:angular-animate@1.3.13"],"client");
 
   api.addFiles([
     /* material-angular base packages */
-    'bower_components/angular-animate/angular-animate.min.js',
     'bower_components/angular-aria/angular-aria.min.js',
-    'bower_components/angular-material/angular-material.min.js',
+    'bower_components/angular-material/angular-material.js',
     'bower_components/angular-material/angular-material.min.css'
   ], "client");
 });
