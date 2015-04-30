f757ac178172ce414324bb44f239ae94bf106ec4
Dominic Garms <djgarms@gmail.com>
2015-04-30 08:34:32 +0800

    update to 0.9.3-rc3 and renamed atmosphere package

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # meteor-angular-material
 This package include angular material as a package (ngMaterial)
+
+## Install
+meteor add angular:angular-material
--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: "Google Material design written for AngularJs (official repo)",
-  name: "angular:material",
-  version: "0.9.0-rc2",
+  name: "angular:angular-material",
+  version: "0.9.0-rc3",
   git: "https://github.com/angular/bower-material.git"
 });
 
