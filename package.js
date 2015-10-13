da2a04e598bd14a05bfbb117bf4fa09829a3a66c
Max Bruchmann <maxbruchmann@gmail.com>
2015-10-14 00:13:47 +0200

    update material

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: "Google Material design written for AngularJs (official repo)",
   name: "angular:angular-material",
-  version: "0.11.2",
+  version: "0.11.4",
   git: "https://github.com/angular/bower-material.git"
 });
 
