b7122580a9811785d042117cd39677bcf0776772
Max Bruchmann <maxbruchmann@gmail.com>
2015-12-09 22:44:49 +0100

    update to rc7

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   summary: "Google Material design written for AngularJs (official repo)",
   name: "angular:angular-material",
-  version: "1.0.0-rc5_1",
+  version: "1.0.0-rc7",
   git: "https://github.com/angular/bower-material.git"
 });
 
 Package.onUse(function (api) {
 
-  api.use(["angular:angular@1.4.6", "angular:angular-animate@1.4.6", "angular:angular-aria@1.4.6"],"client");
+  api.use(["angular:angular@1.4.8", "angular:angular-animate@1.4.8", "angular:angular-aria@1.4.8"],"client");
 
   api.addFiles([
     /* material-angular base packages */
