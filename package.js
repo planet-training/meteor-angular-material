286e557fe34a0ecd3efeeec5eec39a65a8efe855
Dominic Garms <djgarms@gmail.com>
2015-12-15 09:56:34 +0100

    version bump

--- package.js
+++ package.js
@@ -1,14 +1,13 @@
 Package.describe({
   summary: "Google Material design written for AngularJs (official repo)",
   name: "angular:angular-material",
-  version: "1.0.0-rc5_1",
+  version: "1.0.0",
   git: "https://github.com/angular/bower-material.git"
 });
 
 Package.onUse(function (api) {
 
   api.use(["angular:angular@1.4.6", "angular:angular-animate@1.4.6", "angular:angular-aria@1.4.6"],"client");
-
   api.addFiles([
     /* material-angular base packages */
     'bower_components/angular-material/angular-material.js',
